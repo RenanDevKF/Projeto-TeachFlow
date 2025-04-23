036309cabf3401ba079998bd1bd32af9e1ff8c1f
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-22 22:40:49 -0300

    feat: add novos aplicativos e autorizaçao de usurio

--- TeachFlow/teachflow/settings.py
+++ TeachFlow/teachflow/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -97,6 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # Internationalization
