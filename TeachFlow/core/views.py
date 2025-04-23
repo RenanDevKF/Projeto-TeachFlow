27973db832e270a15e492f659134ee080566316b
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-23 20:53:14 -0300

    feat: add view de segurança para dados de professores e outras views que vão herdar essa classe

--- TeachFlow/core/views.py
+++ TeachFlow/core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
 
-# Create your views here.
+from .models import ClassGroup, Student, Lesson, Exercise, Tag, LearningObjective, FutureIdea
+
+
+class TeacherRequiredMixin(UserPassesTestMixin):
+    """Ensure that only teachers can access specific views"""
+    def test_func(self):
+        return hasattr(self.request.user, 'teacher_profile')
+    
+    def handle_no_permission(self):
+        messages.error(self.request, "You must be a teacher to access this page.")
+        return redirect('login')
