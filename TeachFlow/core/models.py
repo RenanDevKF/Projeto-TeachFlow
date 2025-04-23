1b7dcc5598f545075e51cf156b7ddd80c2b0daeb
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-22 22:04:44 -0300

    feat: add modelo de usuario/professor

--- TeachFlow/core/models.py
+++ TeachFlow/core/models.py
@@ -1,4 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
+class Teacher(models.Model):
+    """Teacher profile model extending Django's built-in User"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher_profile')
+    bio = models.TextField(blank=True)
+    subject_area = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+    
