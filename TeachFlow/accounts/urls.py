95854234a886dc9775e8c9369d70576cdc9ae0c7
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-23 22:09:31 -0300

    feat: add urls de accounts

---
+++ TeachFlow/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
+]
