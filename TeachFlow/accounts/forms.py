154c57eb787dd00c5a2c4fc79200c211cfa341e0
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-27 13:44:02 -0300

    feat: criação de formulario de cadastro

---
+++ TeachFlow/accounts/forms.py
@@ -0,0 +1,47 @@
+# accounts/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+from django.core.exceptions import ValidationError
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if CustomUser.objects.filter(username=username).exists():
+            raise ValidationError("Este nome de usuário já está em uso.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise ValidationError("Este email já está em uso")
+        return email
