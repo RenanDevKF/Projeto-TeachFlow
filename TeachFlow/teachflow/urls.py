3e8e3b8b0e5be9064a2aa3f9370c1149acd01f01
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-27 13:54:02 -0300

    fix: correção de urls do projeto django

--- TeachFlow/teachflow/urls.py
+++ TeachFlow/teachflow/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Inclui URLs do app core
-    path('accounts/', include('accounts.urls')),  # URLs de autenticação (se necessário)
+    path('', RedirectView.as_view(pattern_name='dashboard'), name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('', include('core.urls')),
 ]
