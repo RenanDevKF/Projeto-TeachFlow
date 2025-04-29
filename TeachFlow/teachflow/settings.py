ee172107a2f5a89e258076f5ecf4f2e4495c5939
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-04-28 22:10:37 -0300

    fix: correção para horario local

--- TeachFlow/teachflow/settings.py
+++ TeachFlow/teachflow/settings.py
@@ -123,9 +123,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
